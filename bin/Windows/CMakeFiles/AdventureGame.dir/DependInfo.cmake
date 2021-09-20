8cd925b2a28a3aeaebc000c9eedd9a2cec1b614d
Leviathes <lucasholmes275@gmail.com>
2021-09-20 18:07:49 -0400

    mid edit - switching machines

---
+++ bin/Windows/CMakeFiles/AdventureGame.dir/DependInfo.cmake
@@ -0,0 +1,45 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/lucasholmes/DEV/AdventureGame/FileReader/fileReader.cpp" "/Users/lucasholmes/DEV/AdventureGame/bin/Windows/CMakeFiles/AdventureGame.dir/FileReader/fileReader.cpp.o"
+  "/Users/lucasholmes/DEV/AdventureGame/Game/Character_Types/cpp/builder.cpp" "/Users/lucasholmes/DEV/AdventureGame/bin/Windows/CMakeFiles/AdventureGame.dir/Game/Character_Types/cpp/builder.cpp.o"
+  "/Users/lucasholmes/DEV/AdventureGame/Game/cpp/entity.cpp" "/Users/lucasholmes/DEV/AdventureGame/bin/Windows/CMakeFiles/AdventureGame.dir/Game/cpp/entity.cpp.o"
+  "/Users/lucasholmes/DEV/AdventureGame/Game/cpp/game.cpp" "/Users/lucasholmes/DEV/AdventureGame/bin/Windows/CMakeFiles/AdventureGame.dir/Game/cpp/game.cpp.o"
+  "/Users/lucasholmes/DEV/AdventureGame/Game/cpp/main.cpp" "/Users/lucasholmes/DEV/AdventureGame/bin/Windows/CMakeFiles/AdventureGame.dir/Game/cpp/main.cpp.o"
+  "/Users/lucasholmes/DEV/AdventureGame/Game/cpp/player.cpp" "/Users/lucasholmes/DEV/AdventureGame/bin/Windows/CMakeFiles/AdventureGame.dir/Game/cpp/player.cpp.o"
+  "/Users/lucasholmes/DEV/AdventureGame/Game/cpp/scene.cpp" "/Users/lucasholmes/DEV/AdventureGame/bin/Windows/CMakeFiles/AdventureGame.dir/Game/cpp/scene.cpp.o"
+  "/Users/lucasholmes/DEV/AdventureGame/Game/cpp/sprite.cpp" "/Users/lucasholmes/DEV/AdventureGame/bin/Windows/CMakeFiles/AdventureGame.dir/Game/cpp/sprite.cpp.o"
+  "/Users/lucasholmes/DEV/AdventureGame/System/cpp/GUI.cpp" "/Users/lucasholmes/DEV/AdventureGame/bin/Windows/CMakeFiles/AdventureGame.dir/System/cpp/GUI.cpp.o"
+  "/Users/lucasholmes/DEV/AdventureGame/System/cpp/camera.cpp" "/Users/lucasholmes/DEV/AdventureGame/bin/Windows/CMakeFiles/AdventureGame.dir/System/cpp/camera.cpp.o"
+  "/Users/lucasholmes/DEV/AdventureGame/System/cpp/engine.cpp" "/Users/lucasholmes/DEV/AdventureGame/bin/Windows/CMakeFiles/AdventureGame.dir/System/cpp/engine.cpp.o"
+  "/Users/lucasholmes/DEV/AdventureGame/System/cpp/renderer.cpp" "/Users/lucasholmes/DEV/AdventureGame/bin/Windows/CMakeFiles/AdventureGame.dir/System/cpp/renderer.cpp.o"
+  "/Users/lucasholmes/DEV/AdventureGame/System/cpp/texture.cpp" "/Users/lucasholmes/DEV/AdventureGame/bin/Windows/CMakeFiles/AdventureGame.dir/System/cpp/texture.cpp.o"
+  "/Users/lucasholmes/DEV/AdventureGame/System/cpp/textureAtlas.cpp" "/Users/lucasholmes/DEV/AdventureGame/bin/Windows/CMakeFiles/AdventureGame.dir/System/cpp/textureAtlas.cpp.o"
+  "/Users/lucasholmes/DEV/AdventureGame/System/cpp/toolBox.cpp" "/Users/lucasholmes/DEV/AdventureGame/bin/Windows/CMakeFiles/AdventureGame.dir/System/cpp/toolBox.cpp.o"
+  "/Users/lucasholmes/DEV/AdventureGame/System/cpp/vd2d.cpp" "/Users/lucasholmes/DEV/AdventureGame/bin/Windows/CMakeFiles/AdventureGame.dir/System/cpp/vd2d.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/Cellar/sdl2/2.0.16/include/SDL2"
+  "/usr/local/include/SDL2"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
